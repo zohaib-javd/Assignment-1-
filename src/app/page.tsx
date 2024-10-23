e3b9c85fdf5f5a7350d28a0b846b4a04c57a8275
Zohaib Javed <159166949+zohaib-javd@users.noreply.github.com>
2024-10-23 13:23:24 +0500

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 const HomePage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-white p-6 md:flex-row">
@@ -8,15 +10,18 @@ const HomePage = () => {
         </p>
       </div>
       <div className="md:flex-1 flex flex-col justify-center items-center mt-8 md:mt-0">
-        <img 
-          src="/my-image.jpg" 
-          alt="Your Description"
-          className="w-64 h-64 rounded-full object-cover mb-4" 
+        
+        <Image
+          src="/my-image.jpg"
+          alt="Zohaib Javed"
+          width={256}  
+          height={256} 
+          className="rounded-full object-cover mb-4"
         />
         <div className="text-gray-700 text-center">
           <h2 className="text-xl font-semibold">About Me</h2>
           <p className="text-sm">
-          As a seasoned Cloud Applied Generative AI Engineer and banking professional, I bring 13+ years of service quality expertise and a passion for innovation. With a strong background in driving operational excellence and customer satisfaction, I leverage cutting-edge technologies like GenAI, cloud computing, and DevOps to develop intelligent solutions. Proficient in TypeScript, Python, Next.js, and Web3/Blockchain integration, I'm dedicated to transforming financial services through AI-driven innovation.
+            As a seasoned Cloud Applied Generative AI Engineer and banking professional, I bring 13+ years of service quality expertise and a passion for innovation. With a strong background in driving operational excellence and customer satisfaction, I leverage cutting-edge technologies like GenAI, cloud computing, and DevOps to develop intelligent solutions. Proficient in TypeScript, Python, Next.js, and Web3/Blockchain integration, I&#39;m dedicated to transforming financial services through AI-driven innovation.
           </p>
         </div>
       </div>
